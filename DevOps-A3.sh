efa1fe1ce885d2d20ee91783ef5aef8e821c3ad7
Ahmad Makki <148890887+EngrAhmadMakki@users.noreply.github.com>
2023-11-12 16:19:32 +0500

    Bash Script Updated

--- DevOps-A3.sh
+++ DevOps-A3.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# MySQL Database credentials
 DB_USER="root@localhost"
 DB_PASSWORD="mnbvcxz"
 DB_NAME="DevOps_Assignment3"
 
+REMOTE_NAME="gdrive"
+REMOTE_PATH="/DevOps_Database"
+
+BACKUP_DIR="/home/ahmad/DevOps/Backup"
+
+BACKUP_FILENAME="${DB_NAME}_backup_$(date +\%Y\%m\%d_\%H\%M\%S).sql"
+mysqldump -u$DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_DIR/$BACKUP_FILENAME
+
+rclone copy $BACKUP_DIR/$BACKUP_FILENAME $REMOTE_NAME:$REMOTE_PATH
+
+rm $BACKUP_DIR/$BACKUP_FILENAME
