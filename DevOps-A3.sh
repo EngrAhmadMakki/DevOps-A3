b40b641d88a268e9049b4312ee1ecc70e4402f22
Ahmad Makki <148890887+EngrAhmadMakki@users.noreply.github.com>
2023-11-12 16:48:55 +0500

    mysqldump 1698 error removed. Suceesfully run.

--- DevOps-A3.sh
+++ DevOps-A3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DB_USER="root@localhost"
-DB_PASSWORD="mnbvcxz"
+DB_USER="root"
+DB_PASSWORD=""
 DB_NAME="DevOps_Assignment3"
 
 REMOTE_NAME="gdrive"
@@ -10,7 +10,7 @@ REMOTE_PATH="/DevOps_Database"
 BACKUP_DIR="/home/ahmad/DevOps/Backup"
 
 BACKUP_FILENAME="${DB_NAME}_backup_$(date +\%Y\%m\%d_\%H\%M\%S).sql"
-mysqldump -u$DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_DIR/$BACKUP_FILENAME
+sudo mysqldump -u$DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_DIR/$BACKUP_FILENAME
 
 rclone copy $BACKUP_DIR/$BACKUP_FILENAME $REMOTE_NAME:$REMOTE_PATH
 
